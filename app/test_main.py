71a13c1a82c529adcca798385e225fbecbb875b8
Alexm1704 <128852297+Alexm1704@users.noreply.github.com>
2023-03-31 15:10:25 -0500

    Adjusting test

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,8 @@
 from app.main import root
 def test_root(): 
     print(root())
-    assert root() == {"message": "Hello, Alex!!!"}
+    assert root() == {"message": "Hello, smith!!!"}
+
+def test_alex():
+    print(alex())
+    assert alex() == {"message": "Hello, Alex"}
