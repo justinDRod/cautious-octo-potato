0724de34d574d126368027d7ebe2ae90979beedc
justinDRod <Justindrod@yahoo.com>
2023-05-19 11:42:08 -0500

    Enabling cors requests (#14)
    * Enabling cors requests
    
    * changing port
    
    ---------
    
    Co-authored-by: Justin Rodriguez <justindrod@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 import json
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 locations = []
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://justindrod.github.io",
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +19,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root(address: str):
     return locations
